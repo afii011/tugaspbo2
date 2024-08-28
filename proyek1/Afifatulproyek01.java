21ee779c5e34dd37b48d50d267269c7fd6ed0bb2
Afii <afifaukhrowiyah@gmail.com>
2024-08-28 09:44:06 +0700

    Tugas PBO 2

---
+++ proyek1/Afifatulproyek01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.afifatulproyek01;
+
+/**
+ *
+ * @author afifa
+ */
+public class Afifatulproyek01 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Afifatul Ukhrowiyah");
+        System.out.println("NPM    : 2310506011");
+        System.out.println("Alamat : Jepara");
+        System.out.println("No HP  : 000000000000");
+    }
+}
